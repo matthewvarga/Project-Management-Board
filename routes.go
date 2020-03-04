03734fa50a16d1327366c92937863f2e667bdd61
matthewvarga <matthew.l.varga@gmail.com>
2020-03-03 21:57:39 -0500

    added db configuration and insert/select example

---
+++ routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleRoutes(router *mux.Router) {
+
+	// EXAMPLE OF API SUPER BASIC API HANDLER
+	// CAN ALSO USE MIDDLEWARE TO DICTATE WHAT
+	// TYPE OF CALLS THE ENDPOINT ACCEPTS
+	// WITH .Methods("GET", "POST", etc...)
+	// SEE https://github.com/gorilla/mux#examples
+	router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		// an example API handler
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	})
+
+	// ADD OTHER ROUTES HERE
+}
